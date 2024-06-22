022a538ae53ffc9db7fc6c79181cfe940109a498
Dragos.py <dragosesoalca@gmail.com>
2024-06-22 12:01:54 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'W4TCH3R'
+description 'A reward code creation system'
+version '1.0.0'
+
+client_scripts{
+    "client/*.lua"
+}
+
+server_scripts{
+    "server/*.lua"
+}
