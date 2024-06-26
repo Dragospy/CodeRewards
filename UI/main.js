d3681928c44bb742aaa53db687a53bbd502ef32a
Dragos.py <dragosesoalca@gmail.com>
2024-06-26 14:20:55 +0100

    UI has now been made mostly functional and can retrieve search results from the lua script

--- UI/main.js
+++ UI/main.js
@@ -1,10 +1,101 @@
+var Rewards = [];
+var VehiclesFound = [];
+var ItemsFound = [];
 
 window.addEventListener('message', function(event) {
     var item = event.data;
     if (item.type === "ShowUI") {
         $('.Main').show();
     }
+
+    if (item.type === "UpdateRewards") {
+        Rewards = item.rewards
+        var VehicleList = $(".Vehicle-Table").find(".table");
+        var MoneyList = $(".Money-Table").find(".table");
+        var ItemList = $(".Item-Table").find(".table");
+        VehicleList.empty();
+        MoneyList.empty();
+        ItemList.empty();
+        $.each(Rewards, function(id, reward){
+            if (reward.rewardType === "vehicle") {
+                VehicleList.append(`
+                    <tr class="table-row" rewardID = "${id}">
+                        <td class="table-element">${reward.name}</td>
+                    </tr>
+                `);
+            }
+            else if (reward.rewardType === "money") {
+                MoneyList.append(`
+                    <tr class="table-row" rewardID = "${id}">
+                        <td class="table-element">${reward.amount}$</td>
+                    </tr>
+                `);
+            }
+            else if (reward.rewardType === "item"){
+                ItemList.append(`
+                    <tr class="table-row" rewardID = "${id}">
+                        <td class="table-element">${reward.amount} - </td>
+                        <td class="table-element">${reward.label}</td>
+                    </tr>
+                `);
+            }
+        });
+    }
+    if (item.type === "displayVehicles"){
+        VehiclesFound = item.vehicleList
+        var VehicleTable = $(".SearchResults-Vehicles").find(".table");
+        VehicleTable.empty();
+        $.each(VehiclesFound, function(id, vehicle){
+            if (id <= 5) {
+                VehicleTable.append(`
+                    <tr class="table-row-veh" vehicleID = "${id}">
+                        <td class="table-element">${vehicle.name}</td>
+                    </tr>
+                `);
+            }
+        });
+    }
+
+    if (item.type === "displayItems"){
+        ItemsFound = item.itemList
+        var ItemsTable = $(".SearchResults-Items").find(".table");
+        ItemsTable.empty();
+        $.each(ItemsFound, function(id, listItem){
+            if (id <= 5) {
+                ItemsTable.append(`
+                    <tr class="table-row-item" itemID = "${id}">
+                        <td class="table-element">${listItem.label}</td>
+                    </tr>
+                `);
+            }
+        });
+    }
 });
+
+function resetUI(){
+    $('.RewardSelection').show();
+    $('.RewardConfirmation').hide();
+    $('.Vehicles-Section').show();
+    $('.Items-Section').hide();
+    $('.Money-Section').hide();
+    $('#VehicleSearch').val("");
+    $('#MoneyAmount').val("");
+    $('#ItemSearch').val("");
+    var VehicleList = $(".Vehicle-Table").find(".table");
+    var MoneyList = $(".Money-Table").find(".table");
+    var ItemList = $(".Item-Table").find(".table");
+    var VehicleTable = $(".SearchResults-Vehicles").find(".table");
+    var ItemsTable = $(".SearchResults-Items").find(".table");
+    VehicleList.empty();
+    MoneyList.empty();
+    ItemList.empty();
+    VehicleTable.empty();
+    ItemsTable.empty();
+    VehiclesFound = [];
+    ItemsFound = [];
+    Rewards = [];   
+
+}
 
 $(document).on('click', '#VehiclesButton', function() {
     $('.Vehicles-Section').show();
@@ -29,11 +120,66 @@ $(document).on('click', '#Summary', function() {
     $('.RewardConfirmation').show();
 });
 
-$(document).on('click', '.ExitButton', function() {
-    $.post(`https://${GetParentResourceName()}/CloseUI`, JSON.stringify({
-        exit: true
+
+
+$(document).on('click', '#ConfirmVehicle', function() {
+    var text = $('#VehicleSearch').val();
+    $.post(`https://${GetParentResourceName()}/action`, JSON.stringify({
+        action: "SearchVehicles",
+        text: text
     }));
-    $('.RewardSelection').show();
-    $('.RewardConfirmation').hide();
+});
+
+$(document).on('click', '#ConfirmItem', function() {
+    var text = $('#ItemSearch').val();
+    $.post(`https://${GetParentResourceName()}/action`, JSON.stringify({
+        action: "SearchItems",
+        text: text
+    }));
+});
+
+$(document).on('click', '.table-row-veh', function() {
+    var vehicleID = $(this).attr("vehicleID");
+    $.post(`https://${GetParentResourceName()}/action`, JSON.stringify({
+        action: "AddReward",
+        id: 1,
+        rewardType: "vehicle",
+        name: VehiclesFound[vehicleID].name,
+        model: VehiclesFound[vehicleID].model 
+    }));
+    Rewards.push(VehiclesFound[vehicleID]);
+}); 
+    
+$(document).on('click', '.table-row-item', function() {
+        var itemID = $(this).attr("itemID");
+        $.post(`https://${GetParentResourceName()}/action`, JSON.stringify({
+            action: "AddReward",
+            id: 1,
+            rewardType: "item",
+            label: ItemsFound[itemID].label,
+            amount: 1,
+            name: ItemsFound[itemID].name,
+        }));
+        Rewards.push(ItemsFound[itemID]);
+});
+
+$(document).on('click', '#ConfirmMoney', function() {
+    var amount = $('#MoneyAmount').val();
+    $.post(`https://${GetParentResourceName()}/action`, JSON.stringify({
+        action: "AddReward",
+        id: 1,
+        rewardType: "money",
+        amount: amount
+    }));
+});
+
+
+
+
+$(document).on('click', '.ExitButton', function() {
+    $.post(`https://${GetParentResourceName()}/action`, JSON.stringify({
+        action: "CloseUI"
+    }));
+    resetUI();
     $('.Main').hide();
 });
--- client/client.lua
+++ client/client.lua
@@ -1,4 +1,41 @@
 local ui = false
+local rewards = {}
+local QBCore = exports['qb-core']:GetCoreObject()
+
+function findVehicles(searchText)
+    local foundVehicles = {}
+    local text = string.lower(searchText)
+    local count = 0 
+    for k,v in pairs(QBCore.Shared.Vehicles) do
+        if string.find(string.lower(v["name"]), text) ~= nil and count <= 5 then   
+            count = count + 1
+            print(v["name"])   
+            foundVehicles[#foundVehicles+1] = {
+                name = v["name"],
+                model = v["model"],
+            }
+        end
+    end
+    return foundVehicles
+end
+
+function findItems(searchText)
+    local foundItems = {}
+    local text = string.lower(searchText)
+    local count = 0 
+    for k,v in pairs(QBCore.Shared.Items) do
+        if string.find(string.lower(v["label"]), text) ~= nil and count <= 5 then   
+            count = count + 1
+            print(v["label"])   
+            foundItems[#foundItems+1] = {
+                label = v["label"],
+                name = v["name"],
+            }
+        end
+    end
+    print("returned items")
+    return foundItems
+end
 
 RegisterCommand('Rewards', function()
     if not ui then
@@ -10,9 +47,73 @@ RegisterCommand('Rewards', function()
     end
 end, false)
 
-RegisterNUICallback('CloseUI', function(data, cb)
-    SetNuiFocus(false,false)
-    ui = false
+
+RegisterNUICallback('action', function(data, cb)
+    if data.action == 'CloseUI' then
+        rewards = {}    
+        SetNuiFocus(false,false)
+        ui = false
+    end
+
+    if data.action == 'AddReward' then
+        local newReward = {}
+        if data.rewardType == 'vehicle' then
+            newReward = {
+                id = data.id,
+                rewardType = data.rewardType,
+                name = data.name,
+                model = data.model
+            
+            }
+            rewards[#rewards+1] = newReward
+        elseif data.rewardType == 'money' then
+            newReward = {
+                id = data.id,
+                rewardType = data.rewardType,
+                amount = data.amount
+            }
+            rewards[#rewards+1] = newReward
+        elseif data.rewardType == 'item' then
+            newReward = {
+                id = data.id,
+                rewardType = data.rewardType,
+                label = data.label,
+                amount = data.amount,
+                name = data.name
+            }
+            rewards[#rewards+1] = newReward
+        end
+        UpdateRewards()
+    end
+
+    if data.action == "SearchVehicles" then
+        print("Searched")
+
+        local vehicles = findVehicles(data.text)
+        SendNUIMessage({
+            type = "displayVehicles",
+            vehicleList = vehicles
+        })
+    end
+
+    if data.action == "SearchItems" then
+        print("Searched")
+
+        local itemList = findItems(data.text)
+        print("Sending items")
+        SendNUIMessage({
+            type = "displayItems",
+            itemList = itemList
+        })
+        print("sent")
+    end
 end)
+
+function UpdateRewards()
+    SendNUIMessage({
+        type = "UpdateRewards",
+        rewards = rewards
+    })
+end
 
 RegisterKeyMapping('Rewards', 'Opens/Closes UI', 'keyboard', 'L')
